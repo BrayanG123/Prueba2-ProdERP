d5400ff2bb1611ffd7bcf30d323b09240fe3de74
Brayan <brayansamsungfame@gmail.com>
2020-10-09 16:14:44 -0400

    cambiado el link a https

--- src/app/interceptors/interceptor.service.ts
+++ src/app/interceptors/interceptor.service.ts
@@ -13,9 +13,9 @@ export class InterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     console.log('Paso por el interceptor');
-    const secureReq = req.clone({
-      url: req.url.replace('http://', 'https://')
-      });
+    // const secureReq = req.clone({
+    //   url: req.url.replace('http://', 'https://')
+    //   });
     return next.handle( req );
 
   }
